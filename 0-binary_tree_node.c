6cc42cc6c2e84359c58a657bb87ea68193031a91
Franck <franck.spadotto@yahoo.com>
2025-08-04 09:45:37 +0200

    Init first task structure

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,10 @@
+
+/**
+  * binary_tree_node _ 
+  * @parent: Parent to the node to be created.
+  * @value: Value to put in the node.
+  *
+  * Return: 
+  */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
