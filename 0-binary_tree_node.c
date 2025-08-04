878ef34d9d1caadea3fe92d450173535c0d4b7f3
malik31200 <yka-vtc@outlook.fr>
2025-08-04 10:50:05 +0200

    fix syntaxe in file 0-binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,16 +9,18 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newnode;
+	binary_tree_t *new_node;
 
-	newnode = malloc(sizeof(binary_tree_t));
-	if (newnode == NULL)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 	{
 		return (NULL);
 	}
-	newnode->n = value;
-	newnode->parent = parent;
-	newnode->left = NULL;
-	newnode->right = NULL;
-	return (newnode);
+
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
 }
