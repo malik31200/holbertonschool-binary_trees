af83995a858c942523e61f85d6b7e5d5c54d2124
Franck <franck.spadotto@yahoo.com>
2025-08-04 16:29:41 +0200

    Create a func that measure the size of the tree.

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+  * binary_tree_size - Measure the size of the tree from a node.
+  * @tree: Pointer to the root's node.
+  *
+  * Return: Tree's size.
+  */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
