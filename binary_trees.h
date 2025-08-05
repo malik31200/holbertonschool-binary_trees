936be744536681429b650c17f24fb68b5eb3d9ec
Franck <franck.spadotto@yahoo.com>
2025-08-05 13:39:00 +0200

    Func that check if the tree is full or not.

--- binary_trees.h
+++ binary_trees.h
@@ -12,10 +12,7 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-<<<<<<< HEAD
 
-=======
->>>>>>> 21aa51d (Create a func that measure the height of the tree.)
 typedef struct binary_tree_s
 {
 	int n;
@@ -37,29 +34,13 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-<<<<<<< HEAD
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-void binary_tree_print(const binary_tree_t *tree);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-=======
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
-<<<<<<< HEAD
->>>>>>> 21aa51d (Create a func that measure the height of the tree.)
-=======
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
-<<<<<<< HEAD
->>>>>>> af83995 (Create a func that measure the size of the tree.)
-=======
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
->>>>>>> cc1b75e (Create a func that Count the nbr of nodes.)
+int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 
 #endif
